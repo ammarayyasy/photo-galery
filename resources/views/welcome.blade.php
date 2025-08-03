886ae4c31d71a80487ae914a44a149c8d92978a2
ammarayyasy <ammarmojur@gmail.com>
2025-08-03 21:23:19 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Galery Foto Ammar dan Zahra</title>
+
+    <!-- Justified Gallery CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/justifiedGallery/dist/css/justifiedGallery.min.css" />
+
+    <!-- Fancybox CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
+
+<style>
+        body {
+            font-family: sans-serif;
+            background: #fafafa;
+            padding: 2rem;
+        }
+        #gallery {
+            margin: auto;
+        }
+        .jg-entry img {
+            border-radius: 6px;
+        }
+    </style>
+</head>
+<body>
+
+    <h2>Galeri Foto Ammar dan Zahra</h2>
+
+    <div id="gallery">
+        {{-- @foreach ($photos as $photo)
+            <a href="{{ asset('storage/photos/' . $photo->filename) }}">
+                <img src="{{ asset('storage/photos/' . $photo->filename) }}" alt="Foto">
+            </a>
+        @endforeach --}}
+        <a data-fancybox="gallery" href="https://picsum.photos/1280/720">
+            <img src="https://picsum.photos/1280/720" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/1280/720">
+            <img src="https://picsum.photos/1280/720" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/1280/720">
+            <img src="https://picsum.photos/1280/720" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/720/1280">
+            <img src="https://picsum.photos/720/1280" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/1280/720">
+            <img src="https://picsum.photos/1280/720" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/720/1280">
+            <img src="https://picsum.photos/720/1280" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/720/1280">
+            <img src="https://picsum.photos/720/1280" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/1280/720">
+            <img src="https://picsum.photos/1280/720" alt="Foto">
+        </a>
+        <a data-fancybox="gallery" href="https://picsum.photos/1280/720">
+            <img src="https://picsum.photos/1280/720" alt="Foto">
+        </a>
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/justifiedGallery/dist/js/jquery.justifiedGallery.min.js"></script>
+    <script>
+        $(function () {
+            $('#gallery').justifiedGallery({
+                rowHeight: 200,        // semua gambar distandarkan ke tinggi 200px
+                margins: 5,
+                lastRow: 'nojustify',
+                captions: false
+            });
+        });
+    </script>
+
+    <!-- Fancybox JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+    <script>
+        Fancybox.bind("[data-fancybox]", {
+            Thumbs: false,
+            Toolbar: true,
+        });
+    </script>
+
+</body>
+</html>
